77682ac076f3e523c25ae913f773de81dc763a09
DoubleKing <kevin.pqx@icloud.com>
2017-10-19 09:51:14 +0200

    TC3_project

---
+++ TC3_project/chatbot/__init__.py
@@ -0,0 +1,3 @@
+# This package includes two python file
+# One is interface for the chat GUI
+# The other one is to compute the phrase feature and traverse the data-set to search the response which it thinks most correct.
---
+++ TC3_project/chatbot/interface.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import tkinter
+
+if __name__ == '__main__':
+
+    top = tkinter.Tk()
+    # 进入消息循环
+    top.mainloop()
